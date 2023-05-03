ca87225140cd78a03ba4b421c7cd817101ea86a0
MiguelVilela <miguelmvilela12@gmail.com>
2023-05-03 18:10:33 -0300

    Parametrização

--- src/main/java/com/produtos/api_teste/repository/ProdutoRpositorio.java
+++ src/main/java/com/produtos/api_teste/repository/ProdutoRpositorio.java
@@ -1,10 +1,9 @@
 package com.produtos.api_teste.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.produtos.api_teste.model.Produto;
-@Repository
 public interface ProdutoRpositorio extends JpaRepository <Produto, Integer>  {
     
 }
